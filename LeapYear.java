1eed2f45f99d22211ee1204745f90255a83b4a2d
SureshramRajendran <147140664+SureshramRajendran@users.noreply.github.com>
2023-10-12 22:39:06 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -2,7 +2,7 @@ class LeapYear {
 public static void main(String[]args){
 
 int a=2020;
-if(a%4==0){System.out.println("It is Leap Year");}
-else{System.out.println("It is Not Leap Year");}
+if(a%4==0){System.out.println("This Year is Leap Year");}
+else{System.out.println("This Year is Not Leap Year");}
 }
 }
